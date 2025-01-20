1efa3636f51dc65da5fe93a055ac542fcfd7e178
Hyeongra Song <140396278+hrsongg@users.noreply.github.com>
2025-01-20 20:18:45 +0900

    [Bronze II] Title: Strfry, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.cc
+++ 백준/Bronze/11328. Strfry/Strfry.cc
@@ -9,20 +9,23 @@ int main(int argc, char** argv)
 
     int n; // 0 ~ 1001
     cin >> n;
-    string inp, oup;
-    for(int i=0; i<n; i++){
-        cin >> inp;
-        cin >> oup;
-        sort(inp.begin(), inp.end());
-        sort(oup.begin(), oup.end());
+    while(n--){
+        int a[26] = {}; // 각 문자의 개수를 저장하는 배열
+        string s1, s2;
+        cin >> s1 >> s2;
 
-        if(inp == oup){
-            cout << "Possible" << "\n";
+        for(auto c: s1) a[c-'a']++; // s1에서 문자열이 존재하면 +
+        for(auto c: s2) a[c-'a']--; // s2에서 문자열이 존재하면 -
+
+        // 배열 값이 모두 0이면 Possible
+        // 배열 값 중 하나라도 0이 아닌 값이 있으면 Impossible
+        bool isPossible = true;
+        for(int i: a){
+            if(i != 0) isPossible = false;
         }
-        else{
-            cout << "Impossible" << "\n";
-        }
+
+        if(isPossible) cout << "Possible\n";
+        else cout << "Impossible\n";
     }
-    
     return 0;
 }
