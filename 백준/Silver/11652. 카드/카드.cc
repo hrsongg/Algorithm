d2313fb526b391148dcb25c6c11567331db1e8b1
Hyeongra Song <140396278+hrsongg@users.noreply.github.com>
2025-04-08 20:45:33 +0900

    [Silver IV] Title: 카드, Time: 24 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.cc
+++ 백준/Silver/11652. 카드/카드.cc
@@ -3,31 +3,35 @@
 using ll = long long;
 using namespace std;
 
-map <ll, int> mp;
-vector<pair<ll, int>> v;
-int N;
-ll num;
+int N; 
+long long a[100005];
 
-bool cmp(pair<ll, int> p1, pair<ll, int> p2){
-    int cnta = 0, cntb = 0;
-    cnta = p1.second; cntb = p2.second;
-    if(cnta != cntb) return cnta > cntb;
-    // 가장 많이 가지고 있는 정수가 여러 가지라면, 작은 것
-    return p1.first < p2.first;
-}
 
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> N;
-    for(int i=0; i<N; i++){
-        cin >> num;
-        mp[num]++;
-    }
     
-    vector<pair<ll, int>> v(mp.begin(), mp.end());
-    sort(v.begin(), v.end(), cmp);
-    cout << v[0].first;
+    for(int i=0; i<N; i++){
+        cin >> a[i];
+    }
+    sort(a, a+N);
+    int cnt = 0; // 현재 보고 있는 수가 몇번 등장했는지
+    ll mxval = -pow(2, 62)-1; // 현재까지 가장 많이 등장한 수의 값
+    int mxcnt = 0; // 현재까지 가장 많이 등장한 수의 등장 횟수
+    for(int i=0; i<N; i++){
+        if(i==0 || a[i-1] == a[i]) cnt++;
+        // i가 0인 경우, 앞쪽 식이 true이기 때문에 a[i-1]을 참조하지 않음 
+        else{
+            if(cnt > mxcnt){
+                mxcnt = cnt;
+                mxval = a[i-1];
+            }
+            cnt = 1;
+        }
+    }
+    if(cnt > mxcnt) mxval = a[N-1]; // 제일 마지막 수가 몇 번 등장했는지 별도로 확인
+    cout << mxval;
 
     return 0;
 }
