4fff92fd51d6de471fadd945735c1d280df94590
Hyeongra Song <140396278+hrsongg@users.noreply.github.com>
2025-04-10 11:10:09 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int arr[304];
+int dp[304];
+
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    dp[0] = arr[0];
+    dp[1] = arr[0] + arr[1];
+    dp[2] = max(arr[0] + arr[2], arr[1] + arr[2]);
+    for(int i=3; i<n; i++){
+        dp[i] = max(dp[i-2] + arr[i], dp[i-3]+arr[i-1]+arr[i]);
+    }
+    cout << dp[n-1];
+}
